39cf475d1b4f7ebdeca06439ca64e70f2ab309c3
luxx1100 <dpyx201511@foxmail.com>
2018-01-23 21:23:21 +0800

    ‘完善验证功能

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+router.beforeEach((to, from, next) => {
+  let user = localStorage.getItem('mytoken')
+  if (user) {
+    // 允许通过，按照正常的逻辑向下调换
+    next()
+  } else {
+    if (to.path !== '/login') {
+      next({path: '/login'})
+    } else {
+      next()
+    }
+  }
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: c => c(App)
+})
