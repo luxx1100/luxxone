9d099978c9d604a4eb02428bb2d6389539c473ca
luxx1100 <dpyx201511@foxmail.com>
2018-01-24 20:21:47 +0800

    完善表单验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,22 @@
 <template>
-    <div>
-      <h1>Login</h1>
-      <ul>
-        <li>
-          <label for="uname">用户名：</label>
-          <input type="text" v-model="username" id="uname">
-        </li>
-        <li>
-        <label for="pw">密码</label>
-        <input type="text" v-model="password" id="pw">
-        </li>
-        <li>
-          <input type="button" value="提交" @click="login">
-        </li>
-      </ul>
+    <div class="info">
+      <div class="avatar">
+        <img src="../assets/logo.png">
+      </div>
+      <!-- ref表示 组件名称，用于直接操作dom， -->
+      <el-form ref="Loginform" :rules="rules" :model="form" class="container">
+        <div class="userInfo">
+            <el-form-item prop="username">
+            <el-input prefix-icon="myicon myicon-user" placeholder="账号" v-model="form.username"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input prefix-icon="myicon myicon-key" placeholder="密码" type="password" v-model="form.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="login">登录</el-button>
+          </el-form-item>
+        </div>
+        </el-form>
     </div>
 </template>
 
@@ -22,22 +25,40 @@ import {login} from '../api/api.js'
 export default {
   data () {
     return {
-      username: '',
-      password: ''
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'}
+        ]
+      }
     }
   },
   methods: {
     login: function () {
-      let params = {
-        username: this.username,
-        password: this.password
-      }
-      login(params).then(res => {
-        if (res.meta.status === 200) {
-          localStorage.setItem('mytoken', res.data.token)
-          this.$router.push({name: 'home'})
-        } else {
-          console.log('用户名或密码错误')
+      // 调用接口之前做表单验证
+      this.$refs['Loginform'].validate(valid => {
+        if (valid) {
+          let params = {
+            username: this.form.username,
+            password: this.form.password
+          }
+          login(params).then(res => {
+            if (res.meta.status === 200) {
+              localStorage.setItem('mytoken', res.data.token)
+              this.$router.push({name: 'home'})
+            } else {
+              this.$message({
+                message: res.meta.msg,
+                type: 'error'
+              })
+            }
+          })
         }
       })
     }
@@ -47,5 +68,50 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-sizing: border-box;
+    background-color: #2f4050;
+  }
+  .container {
+    box-sizing: border-box;
+    margin: 180px auto 0;
+    width: 500px;
+    height: 340px;
+    background-color: #fff;
+    position: relative;
+  }
+  .avatar {
+    position: absolute;
+    left: 50%;
+    top: 110px;
+    width: 120px;
+    height: 120px;
+    border: 10px solid #FFF;
+    margin-left: -70px;
+    border-radius: 50%;
+    box-shadow: 0 1px 5px #CCC;
+    background-color: pink;
+    overflow: hidden;
+    z-index: 9999;
+  }
+  .userInfo {
+    position: absolute;
+    text-align: center;
+    width: 400px;
+    margin: 100px 50px;
+  }
+  .btn {
+    width: 400px;
+  }
+  /*.el-form-item {
+    color: blue;
+  }*/
+  .el-form-item__error {
+    color: blue;
+  }
 </style>
